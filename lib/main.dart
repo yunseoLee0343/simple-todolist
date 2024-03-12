a8b2b1a39b2d4ee7c3e7955f4ad450708707f20c
hin6150 <hin6150@gmail.com>
2024-03-12 18:07:37 +0900

    :bento: Font/Color 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,17 @@
 import 'package:flutter/material.dart';
 
+// 배경1: #FF8181
+// 폰트1: #077522
+
+// 배경2: #FCE38A
+// 폰트2: #6677BB
+
+// 배경3: #EAFFD0
+// 폰트3: #BA55D3
+
+// 배경4: #95E1D3
+// 폰트4: #569889
+
 void main() {
   runApp(const MyApp());
 }
