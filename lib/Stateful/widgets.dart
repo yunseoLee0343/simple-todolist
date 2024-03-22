9e5caa91a04f536156c8498e798e255aefb80310
Yunseo Lee <lys139011@gmail.com>
2024-03-22 13:37:09 +0900

    feat : 홈 세부리스트 구현

--- lib/Stateful/widgets.dart
+++ lib/Stateful/widgets.dart
@@ -1,3 +1,5 @@
+import 'package:dorun_2th_flutter_study/Stateful/models.dart';
+import 'package:dorun_2th_flutter_study/Stateful/screens.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -11,6 +13,9 @@ class TodoCard extends StatefulWidget {
   final Function(int, int) updateFlexValue;
   final Function(int, bool) updateSubTitle;
 
+  final List items;
+  final List subItems;
+
   TodoCard({
     super.key,
     required this.title,
@@ -21,6 +26,8 @@ class TodoCard extends StatefulWidget {
     required this.flexVals,
     required this.updateFlexValue,
     required this.updateSubTitle,
+    required this.items,
+    required this.subItems,
   });
 
   @override
@@ -29,9 +36,11 @@ class TodoCard extends StatefulWidget {
 
 class _TodoCardState extends State<TodoCard> {
   bool flexed = false;
+  bool _offstage = true;
 
   @override
   Widget build(BuildContext context) {
+
     return GestureDetector(
       behavior: HitTestBehavior.translucent,
       onTap: () {
@@ -40,6 +49,11 @@ class _TodoCardState extends State<TodoCard> {
 
         if(flexed) widget.updateFlexValue(widget.index, 4); //5 이상일 경우 Overflow 발생. 이슈 등록 필요.
         else widget.updateFlexValue(widget.index, 1);
+
+        // show TodoTexts
+        setState(() {
+          _offstage = !_offstage;
+        });
       },
       child: Container(
         color: widget.color,
@@ -58,7 +72,14 @@ class _TodoCardState extends State<TodoCard> {
               ),
               trailing: IconButton(
                 icon: const Icon(Icons.add),
-                onPressed: () => context.go('/create'),
+                onPressed: () => context.goNamed("create", pathParameters: {'items': widget.items.join(","), 'subItems': widget.subItems.join("|")}),
+              ),
+            ),
+
+            Expanded(
+              child: Offstage(
+                offstage: _offstage,
+                child: TodoList(cardIdx: widget.index),
               ),
             ),
           ],
@@ -68,3 +89,38 @@ class _TodoCardState extends State<TodoCard> {
   }
 }
 
+class TodoList extends StatefulWidget {
+  final int cardIdx;
+  const TodoList({required this.cardIdx});
+
+  @override
+  State<StatefulWidget> createState() => _TodoTextState();
+}
+
+class _TodoTextState extends State<TodoList> {
+  late List<ToDoItem> _items;
+  bool _isToggled = false;
+
+  @override
+  Widget build(BuildContext context) {
+    _items = Repository.getItems(widget.cardIdx);
+
+    return ListView.builder(
+      itemCount: _items.isEmpty ? 1 : _items.length,
+      itemBuilder: (context, index) => _items.isEmpty
+          ? Text("ALL DONE!\nClick + Button To Add ToDo Items!", style: TextStyle(fontSize: 17),)
+          : TextButton(
+              child: Text(_items[index].content, style: TextStyle(fontSize: 17, decoration: _isToggled ? TextDecoration.lineThrough : null,),),
+              onPressed: () {
+                setState(() => _isToggled = !_isToggled);
+                Repository.updateItem(index, _items[index].content, ToDoItemState.strikeouted);
+              },
+              onLongPress: () {
+                Repository.deleteItem(index, _items[index].content);
+              },
+              style: TextButton.styleFrom(alignment: Alignment.centerLeft,),
+            ),
+      padding: EdgeInsets.all(10),
+    );
+  }
+}
